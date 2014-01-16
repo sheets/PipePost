3f6042bb3b4e90594b954d8c98bda1ff7dc56ca1
vivek <vivek@vivek-laptop.(none)>
2014-01-17 02:40:50 +0530

    [all setting]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :check_plan,:only=>[:new,:create]
+  before_filter :create_customer,:only=>[:create]
+  require "rubygems"
+  require "braintree"
+  Braintree::Configuration.environment = :sandbox
+  Braintree::Configuration.merchant_id = "t99rmv6yyz6k2xvm"
+  Braintree::Configuration.public_key = "x6h7w7tyqt7nm9f8"
+  Braintree::Configuration.private_key = "23c60ed7187c772802307c1f2d06d8bd"
   def new
   	@title="Sign up"
     @breadcrumb=@title
@@ -9,16 +16,28 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    flash[:alert]="This page is still under process"
-    redirect_to(:back)
-  	# attributeUserProfile = params[:user][:userprofile]
-  	# params[:user]=params[:user].except(:userprofile)
-  	# @title="Sign up"
-    #  @breadcrumb=@title
-    #  @description="Lorem ipsum dolor sit amet"
-    #  super
-    #  attributeUserProfile[:user_id]=@user.id
-    #  @userProfile=Userprofile.create!(attributeUserProfile) rescue "" 
+    debugger
+    
+    result = Braintree::Subscription.create(:credit_card => cardDetail,:plan_id => "#{@plan.plan_key}")
+
+    if result.success?
+      puts result.subscription.id
+      puts result.subscription.transactions[0].id
+      attributeUserProfile = params[:user][:userprofile]
+      params[:user]=params[:user].except(:userprofile,:card_name,:cvv,:exp_date)
+      @title="Sign up"
+      @breadcrumb=@title
+      @description="Lorem ipsum dolor sit amet"
+      super
+      attributeUserProfile[:user_id]=@user.id
+      @userProfile=Userprofile.create!(attributeUserProfile) rescue "" 
+    else
+      result
+      redirect_to(:back)
+    end
+
+
+    #  flash[:alert]="This page is still under process"
   end
 
   def update
@@ -33,6 +52,14 @@ class RegistrationsController < Devise::RegistrationsController
       flash[:notice]="Please select plan to signup"
     else
       @plan = findPlan
+    end
+  end
+  def create_customer
+    cardDetail={:number=>params[:user][:card_name],:expiration_date=>params[:user][:expiration_date],:cvv=>params[:user][:cvv]}
+    result = Braintree::Customer.create(:first_name =>params[:user][:first_name],:last_name => params[:user][:last_name],:credit_card =>cardDetail)
+    unless result.success?
+      flash[:alert]="Error: #{result.message}"
+      redirect_to(:back)
     end
   end
 end 
