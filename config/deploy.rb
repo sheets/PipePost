bfde816bc9d9336ba16f2a52cd5cf57b7d3eb95d
vivek <vivek@vivek-laptop.(none)>
2014-02-06 03:18:51 +0530

    [assets tar]

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,23 +63,24 @@ namespace :deploy do
     task :precompile, :roles => :web do
       from = source.next_revision(current_revision)
       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-        run_locally("rake assets:clean && rake assets:precompile")
-        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        # run_locally("rake assets:clean && rake assets:precompile")
+        # run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        # run "cd #{shared_path} && rm assets.tar.bz2"
         top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
         run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
         run_locally "rm public/assets.tar.bz2"
-        run_locally("rake assets:clean")
+        run_locally("rake assets:clean")  
       else
         logger.info "Skipping asset precompilation because there were no asset changes"
       end
     end
 
-    task :symlink, roles: :web do
-      run ("rm -rf #{latest_release}/public/assets &&
-            mkdir -p #{latest_release}/public &&
-            mkdir -p #{shared_path}/assets &&
-            ln -s #{shared_path}/assets #{latest_release}/public/assets")
-    end
+    # task :symlink, roles: :web do
+    #   run ("rm -rf #{latest_release}/public/assets &&
+    #         mkdir -p #{latest_release}/public &&
+    #         mkdir -p #{shared_path}/assets &&
+    #         ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    # end
   end
 
 
